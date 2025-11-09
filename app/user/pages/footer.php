a24618e68167e6a4e995125150013019113feccb
devinnahr <devinnahr81@gmail.com>
2025-11-09 23:07:50 +0700

    skripsi

---
+++ app/user/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>SMP NEGERI 1 KAUMAN</b> 
+    </div>
+   <strong>Copyright &copy; <?= date('Y'); ?>. </strong> Sistem Informasi Perpustakaan Spensaka.
+</footer>
